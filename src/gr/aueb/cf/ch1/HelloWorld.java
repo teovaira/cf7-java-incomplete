9f55381947ace964e31c02f0da59667496b70813
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-01-03 13:04:00 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints Hello Coding Factory in
+ * standard output (console).
+ *
+ * @author a8ana
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints Hello Coding Factory
+
+        /*
+         * Prints in standard output
+         * "Hello Coding Factory"
+         */
+        System.out.println("Hello Coding Factory!");    // Prints Hello Coding Factory
+        System.out.print("Hello World!");
+        System.out.println();
+    }
+}
